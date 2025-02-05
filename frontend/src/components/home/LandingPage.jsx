331f6ebd1f743a59d5403f7e9c405fc7f03ffd8a
nikhilraj012 <nikhilraj@estavel.ai>
2025-02-05 14:41:24 +0530

    updated landingpage

--- frontend/src/components/home/LandingPage.jsx
+++ frontend/src/components/home/LandingPage.jsx
@@ -170,7 +170,7 @@ const LandingPage = () => {
           </div>
           <a
             href="#"
-            className="relative inline-flex items-center justify-center lg:px-8 lg:py-2 overflow-hidden font-mono font-medium tracking-tighter text-white border-2 border-[#FBAD30] rounded-lg group"
+            className="relative inline-flex items-center justify-center p-1 md:px-8 md:py-2 overflow-hidden font-mono font-medium tracking-tighter text-white border-2 border-[#FBAD30] rounded-lg group"
           >
             <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-[#FBAD30] rounded-full group-hover:w-80 group-hover:h-56"></span>
             <span className="absolute inset-0 w-full h-full rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
